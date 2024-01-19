61633da8f35f476e54b622aa5dcd0add2b6671fb
Dev <dev@xxtime.com>
2024-01-19 11:00:11 +0800

    page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,17 @@
 // var QRCode = require('qrcode')
 import QRCode from 'qrcode'
 
-var opts = {
-    type: "svg",
-    mode: 'alphanumeric',
-    version: 3,
-    errorCorrectionLevel: "M",
-    margin: 4,
-    color: {
-        dark: "#000000ff",
-        light: "#ffffffff",
-    },
-}
+const opts = {
+  type: "svg",
+  mode: 'alphanumeric',
+  version: 3,
+  errorCorrectionLevel: "M",
+  margin: 4,
+  color: {
+    dark: "#000000ff",
+    light: "#ffffffff",
+  },
+};
 
 let canvas = document.getElementById('canvas')
 let img = document.getElementById('img_qr')
@@ -19,14 +19,14 @@ let qrText = "https://zlab.dev"
 
 // canvas
 QRCode.toCanvas(canvas, qrText, function (error) {
-    if (error) console.error(error)
-    console.log('success');
+  if (error) console.error(error)
+  console.log('success');
 })
 
 // data:image/png;base64
 QRCode.toDataURL(qrText, function (err, url) {
-    img.setAttribute('src', url)
-    console.log(url)
+  img.setAttribute('src', url)
+  console.log(url)
 })
 
 // svg file data
